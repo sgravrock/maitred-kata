730d03e6e5ea0268d3550761a3ff6542b9cf40b1
Steve Gravrock <sgravrock@pivotal.io>
2021-02-25 10:39:16 -0800

    Boutique: table size is configurable

--- src/main/java/maitredkata/MaitreD.java
+++ src/main/java/maitredkata/MaitreD.java
@@ -4,7 +4,12 @@ import java.util.Date;
 
 
 public class MaitreD {
+    private int _tableSize;
+
+    public MaitreD(int tableSize) {
+        _tableSize = tableSize;
+    }
     public boolean reserve(Date date, int qty) {
-        return qty <= 12;
+        return qty <= _tableSize;
     }
 }
--- src/test/java/maitredkata/MaitreDTest.java
+++ src/test/java/maitredkata/MaitreDTest.java
@@ -9,20 +9,26 @@ import static org.junit.Assert.*;
 public class MaitreDTest {
     @Test
     public void acceptsSingleReservationBelowTableSize() {
-        MaitreD subject = new MaitreD();
+        MaitreD subject = new MaitreD(12);
         assertTrue(subject.reserve(arbitraryDate(), 1));
     }
 
     @Test
     public void acceptsSingleReservationAtTableSize() {
-        MaitreD subject = new MaitreD();
+        MaitreD subject = new MaitreD(12);
         assertTrue(subject.reserve(arbitraryDate(), 12));
     }
 
     @Test
     public void rejectsSingleReservationAboveTableSize() {
-        MaitreD subject = new MaitreD();
+        MaitreD subject = new MaitreD(12);
         assertFalse(subject.reserve(arbitraryDate(), 13));
+    }
+
+    @Test
+    public void usesConfiguredTableSize() {
+        MaitreD subject = new MaitreD(13);
+        assertTrue(subject.reserve(arbitraryDate(), 13));
     }
 
     private static Date arbitraryDate() {
