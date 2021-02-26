d30d7445058ff4809922956eacbd37c8f157a406
Steve Gravrock <sgravrock@pivotal.io>
2021-02-25 17:07:03 -0800

    Introduced a Reservation class

--- src/main/java/maitredkata/HauteCuisineMaitreD.java
+++ src/main/java/maitredkata/HauteCuisineMaitreD.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class HauteCuisineMaitreD {
     private final List<Integer> _tableSizes;
-    private final DayMap<List<Integer>> _reservations;
+    private final DayMap<List<Reservation>> _reservations;
 
     public HauteCuisineMaitreD(int... tableSizes) {
         _tableSizes = new ArrayList<>();
@@ -18,15 +18,16 @@ public class HauteCuisineMaitreD {
     }
 
     public boolean reserve(Date date, int qty) {
-        if (!canReserve(date, qty)) {
+        Reservation reservation = new Reservation(qty);
+        if (!canReserve(date, reservation)) {
             return false;
         }
 
-        _reservations.get(date).add(qty);
+        _reservations.get(date).add(reservation);
         return true;
     }
 
-    private boolean canReserve(Date date, int qty) {
+    private boolean canReserve(Date date, Reservation newReservation) {
         // Sort the table sizes so we assign each party to the smallest table
         // that fits. This avoids e.g. a party of 4 not being able to be seated
         // because a party of 2 was previously seated at the only 4 top when
@@ -34,11 +35,11 @@ public class HauteCuisineMaitreD {
         List<Integer> unassignedTables = _tableSizes.stream()
                 .sorted()
                 .collect(Collectors.toList());
-        List<Integer> reservationsIncludingNew = new ArrayList<>(_reservations.get(date));
-        reservationsIncludingNew.add(qty);
+        List<Reservation> reservationsIncludingNew = new ArrayList<>(_reservations.get(date));
+        reservationsIncludingNew.add(newReservation);
 
-        for (Integer rq: reservationsIncludingNew) {
-            int i = findFirstGreaterThanOrEqual(unassignedTables, rq);
+        for (Reservation res: reservationsIncludingNew) {
+            int i = findFirstGreaterThanOrEqual(unassignedTables, res.getNumDiners());
 
             if (i == -1) {
                 return false;
---
+++ src/main/java/maitredkata/Reservation.java
@@ -0,0 +1,13 @@
+package maitredkata;
+
+public class Reservation {
+    private final int _numDiners;
+
+    public Reservation(int numDiners) {
+        _numDiners = numDiners;
+    }
+
+    public int getNumDiners() {
+        return _numDiners;
+    }
+}
