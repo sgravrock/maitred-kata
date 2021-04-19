345853dc3b5146180615a2f1e9a22bd26b5ca25c
Steve Gravrock <sgravrock@pivotal.io>
2021-04-19 10:44:01 -0700

    Revert "Inherit all the things"
    This reverts commit 3df16f918e2bee3498cdf01a8abc115a06178571.

--- src/main/java/maitredkata/DayMap.java
+++ src/main/java/maitredkata/DayMap.java
@@ -3,13 +3,24 @@ package maitredkata;
 import java.time.LocalDate;
 import java.util.*;
 
-public class DayMap extends DefaultingMap<LocalDate, List<Reservation>> {
+public class DayMap {
+    private Map<LocalDate, List<Reservation>> _storage;
+
+    public DayMap() {
+        _storage = new HashMap<>();
+    }
+
     public void addReservation(Reservation r) {
         get(r.getDate()).add(r);
     }
 
-    @Override
-    protected List<Reservation> makeDefaultValue() {
-        return new ArrayList<>();
+    public List<Reservation> get(LocalDate key) {
+        if (_storage.containsKey(key)) {
+            return _storage.get(key);
+        } else {
+            List<Reservation> value = new ArrayList<>();
+            _storage.put(key, value);
+            return value;
+        }
     }
 }
